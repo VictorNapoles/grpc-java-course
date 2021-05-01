190d2a69541111e407c0223963bc5a579da91877
Victor de Nápoles dos Santos <CIANDT\vnapoles@lnb028807bhz.ciandt.global>
2021-05-01 10:18:07 -0300

    Calculator service

--- src/main/java/com/github/simplesteph/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/github/simplesteph/grpc/calculator/client/CalculatorClient.java
@@ -1,16 +1,19 @@
 package com.github.simplesteph.grpc.calculator.client;
 
-import com.proto.calculator.Calculator;
-import com.proto.calculator.CalculatorRequest;
-import com.proto.calculator.CalculatorResponse;
-import com.proto.calculator.CalculatorServiceGrpc;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.proto.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import java.util.Iterator;
 
 public class CalculatorClient {
     public static void main(String[] args) {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+
         CalculatorServiceGrpc.CalculatorServiceBlockingStub service = CalculatorServiceGrpc.newBlockingStub(channel);
+
+        /*
         Calculator calculator = Calculator.newBuilder()
                 .setNumber1(3)
                 .setNumber2(10)
@@ -21,7 +24,17 @@ public class CalculatorClient {
                 .build();
 
         CalculatorResponse response = service.calculate(request);
+
         System.out.println(response.getResult());
 
+        */
+        PrimeNumberDecomposition number = PrimeNumberDecomposition.newBuilder().setNumber(9872621).build();
+        PrimeNumberDecompositionRequest request = PrimeNumberDecompositionRequest.newBuilder().setRequest(number).build();
+        Iterator<PrimeNumberDecompositionResponse> results = service.primeNumberDecomposition(request);
+        results.forEachRemaining(res ->{
+            System.out.println(res.getResult());
+        });
+
+        channel.shutdown();
     }
 }
--- src/main/java/com/github/simplesteph/grpc/calculator/server/CalculatorServiceImpl.java
+++ src/main/java/com/github/simplesteph/grpc/calculator/server/CalculatorServiceImpl.java
@@ -1,9 +1,6 @@
 package com.github.simplesteph.grpc.calculator.server;
 
-import com.proto.calculator.Calculator;
-import com.proto.calculator.CalculatorRequest;
-import com.proto.calculator.CalculatorResponse;
-import com.proto.calculator.CalculatorServiceGrpc;
+import com.proto.calculator.*;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
@@ -15,6 +12,31 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         CalculatorResponse response = CalculatorResponse.newBuilder().setResult(result).build();
 
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void primeNumberDecomposition(PrimeNumberDecompositionRequest request, StreamObserver<PrimeNumberDecompositionResponse> responseObserver) {
+        Integer number = request.getRequest().getNumber();
+        
+        boolean hasNext = true;
+        
+        while (hasNext){
+            for (int i = 2; i <=number ; i++) {
+                if(number % i ==0){
+                    PrimeNumberDecompositionResponse response = PrimeNumberDecompositionResponse.newBuilder().setResult(i).build();
+                    responseObserver.onNext(response);
+
+                    number = number / i;
+
+                    if(number == 1){
+                        hasNext = false;
+                    }
+                    break;
+                }
+            }
+        }
+        
         responseObserver.onCompleted();
     }
 }
